930c0c5cbc456954565a8771747abe194cb55df4
BurCha <1477996221@qq.com>
2020-10-14 23:21:47 +0800

    Initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,6 @@
+<<<<<<< Updated upstream
 console.log("感谢您的浏览，期待能够加入贵公司！我的博客：http://if2er.com/");
+=======
+console.log("我的博客：http://www.burc.com.cn/#/ 【服务器到期时间 2021-08-16 00:00:00】");
+console.log("我的博客园：https://www.cnblogs.com/burc/");
+>>>>>>> Stashed changes
